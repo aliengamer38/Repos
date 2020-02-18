f6f46b137094571278a8ef681a06aa018ae7065f
AlienGamer38 <angelosevilleno38@outlook.com>
2020-02-18 10:09:10 +0800

    IO net app

--- NetApp/ConsoleUI/Program.cs
+++ NetApp/ConsoleUI/Program.cs
@@ -1,25 +1,83 @@
-﻿using System;
+﻿using CorePOSLibrary;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
+using UI = ConsoleUI;
+
 
 namespace ConsoleUI
 {
-    class Program
+    public class Execution
     {
-        static void Main(string[] args)
-        {            
-            string s = "";
-            void Change()
-            {
-                s = "Changed";
-            }
-            Change();
-            Console.WriteLine(s);
+        public List<Employee> Employees { get; set; } = new EmployeeDbContext().Employees;
 
+        public Execution()
+        {
+            Console.WriteLine("Execution constructor");
         }
+    }
+
+    public class Engine
+    {
+        Lazy<Execution> _exec;
+        public Engine()
+        {
+            _exec = new Lazy<Execution>(() =>
+            {
+                Console.WriteLine("Default constructor is fired");
+                return new Execution();
+            });
+        }
+        public Execution Helper()
+        {
+            return _exec.Value;
+        }
+    }
+    public class Program
+    {
         
+            
+    }
+
+
+    public class EmployeeComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class EmployeeEqualityComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            string xName = $"{x.FirstName} {y.LastName}";
+            string yName = $"{x.FirstName} {y.LastName}";
+            return xName.Equals(yName);
+        }
+
+        public int GetHashCode(Employee emp)
+        {
+            return $"{emp.FirstName} {emp.LastName}".GetHashCode();
+        }
+    }
+
+    public class EmpBoolComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return x.IsCurrentlyEnrolled == y.IsCurrentlyEnrolled;
+        }
+
+        public int GetHashCode(Employee emp)
+        {
+            return $"{emp.FirstName} {emp.LastName}".GetHashCode();
+        }
     }
 }
---
+++ NetApp/ConsoleUI/Supply.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleUI
+{ 
+    [Serializable]
+    public class Supply
+    {       
+
+        public ref Computer ChangeNumber(ref Computer comp, Computer c)
+        {
+            comp = new Computer() { SerialNumber = 1000 };
+            return ref comp;
+        }
+
+        static ref int Helper(int[] arr, int i)
+        {
+            return ref arr[i];
+        }
+        void Work()
+        {
+            Console.WriteLine("WORK");
+        }
+    }
+
+    public class Computer
+    {
+        public int SerialNumber { get; set; }
+        public string Brand { get; set; }
+    }
+}
